56775f68f4fe9b220e9c75fb7396b6eb34a94842
racheallarimer <racheal.larimer@gmail.com>
2021-04-15 13:29:39 -0700

    complete placeShip function for computerBoard

--- src/utils/computerBoardFactory.js
+++ src/utils/computerBoardFactory.js
@@ -1,4 +1,3 @@
-import Ship from '../components/Ship'
 import createBoard from './gameboardFactory'
 import shipFactory from './shipFactory'
 
@@ -6,41 +5,51 @@ const getRandomNumber = (length) => {
   return Math.round(Math.random() * (9 - length))
 }
 
-const getCellsToFill = (column, length) => {
-  const startIndex = getRandomNumber(length)
-  return column.slice(startIndex, startIndex + length)
-}
-
-const getValidPlacement = (board, ship) => {
-  let selectedColumn = board.columns[Object.keys(board.columns)[getRandomNumber(0)]]
-  let shipFits = true
-  const selectedCells = getCellsToFill(selectedColumn, ship.pieces.length)
-  selectedCells.forEach(cell => {
-    if (cell.shipPiece !== '') { shipFits = false}
-  }) 
-  if (shipFits) {
-    placeShip(ship, selectedCells)
-  } else {
-    getValidPlacement(board, ship.pieces.length)
-  }
-}
-
-const placeShip = (ship, cells) => {
-  console.log(ship, cells)
-  ship.pieces.forEach((piece, index) => {
-    let newCell = {
-      ...cells[index],
-      shipPiece: piece
-    }
-  })
-}
-
 const computerBoard = () => {
   let computerBoard =  createBoard()
   let ships = shipFactory()
 
+  const getCellsToFill = (column, length) => {
+    const startIndex = getRandomNumber(length)
+    return column.slice(startIndex, startIndex + length)
+  }
+  
+  const getValidPlacement = (ship) => {
+    let selectedColumn = computerBoard.columns[Object.keys(computerBoard.columns)[getRandomNumber(0)]]
+    let shipFits = true
+    const selectedCells = getCellsToFill(selectedColumn, ship.pieces.length)
+    selectedCells.forEach(cell => {
+      if (cell.shipPiece !== '') { shipFits = false}
+    }) 
+    if (shipFits) {
+      placeShip(ship, selectedCells)
+    } else {
+      getValidPlacement(ship)
+    }
+  }
+  
+  const placeShip = (ship, cells) => {
+    console.log(ship, cells)
+    const columnId = [cells[0].id[0]]
+    let newColumn = computerBoard.columns[columnId].slice()
+    ship.pieces.forEach((piece, index) => {
+      let newCell = {
+        ...cells[index],
+        shipPiece: piece
+      }
+      newColumn.splice(newColumn.findIndex(cell => cell.id === cells[index].id), 1, newCell)
+    })
+
+    computerBoard = {
+      columns: {
+        ...computerBoard.columns,
+        [columnId]: newColumn
+      }
+    }
+  }
+
   ships.forEach((ship) => {
-    getValidPlacement(computerBoard, ship)
+    getValidPlacement(ship)
   })
 
   return computerBoard
